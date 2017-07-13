31bc5268e7a4b9945e59e8e9bbce6ef7351b97d6
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2017-07-13 08:21:42 +0200

    Solved!

--- warmup - simple array sum.java
+++ warmup - simple array sum.java
@@ -1,23 +1,19 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
-    static int simpleArraySum(int n, int[] ar) {
-        // Complete this function
-    }
-
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.nextInt();
+        long sum = 0;
+        while (scanner.hasNextInt()) {
+            sum += scanner.nextInt();
         }
-        int result = simpleArraySum(n, ar);
-        System.out.println(result);
+
+        scanner.close();
+
+        System.out.println(sum);
     }
 }
